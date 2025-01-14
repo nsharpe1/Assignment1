9a7e00ed608ab9a838954085d3722d44599b43e5
nsharpe1 <nicholas.sharpe@keyin.com>
2025-01-14 11:02:57 -0330

    First commit

---
+++ src/TemperatureAboveAverage.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+public class TemperatureAboveAverage {
+    int arr[] = null;
+
+    public TemperatureAboveAverage(int sizeofArray){
+        arr = new int[sizeofArray];
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = Integer.MIN_VALUE;
+        }
+    }
+
+    public void insert (int location, int valueEntered){
+        try {
+            if (arr[location] == Integer.MIN_VALUE) {
+                arr[location] = valueEntered;
+                System.out.println("Successfully entered");
+            } else {
+                System.out.println("The location is already occupied");
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index top access array");
+        }
+    }
+
+    public static void main(String[] args) {
+        TemperatureAboveAverage temperatureAboveAverage = new TemperatureAboveAverage(5);
+        temperatureAboveAverage.insert(0,23);
+        temperatureAboveAverage.insert(1,26);
+        temperatureAboveAverage.insert(2,29);
+        temperatureAboveAverage.insert(3,32);
+        temperatureAboveAverage.insert(4,35);
+        int length = temperatureAboveAverage.arr.length;
+        int sum = 0;
+        for (int i = 0; i < temperatureAboveAverage.arr.length; i++) {
+            sum += temperatureAboveAverage.arr[i];
+        }
+        double average = sum / length;
+        int abovetemperatureAverage = 0;
+        for (int i = 0; i < temperatureAboveAverage.arr.length; i++) {
+            if (temperatureAboveAverage.arr[i]>average) {
+                abovetemperatureAverage++;
+            }
+        }
+        
+        System.out.println("Average temperature = " + average);
+        System.out.println(abovetemperatureAverage + " numbers are above the average temperature");
+        System.out.println(Arrays.toString(temperatureAboveAverage.arr));
+    }
+}
